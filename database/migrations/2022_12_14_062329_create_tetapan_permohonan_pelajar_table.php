7c4dab549acdec348f8a5d8fd588f2a3b2cdbabe
SyafiqCheNor <syafiqchenor@gmail.com>
2022-12-14 14:26:12 +0800

    typo migration

---
+++ database/migrations/2022_12_14_062329_create_tetapan_permohonan_pelajar_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tetapan_permohonan_pelajar', function (Blueprint $table) {
+            $table->id();
+            $table->integer('kursus_id')->nullable();
+            $table->string('sesi_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tetapan_permohonan_pelajar');
+    }
+};
