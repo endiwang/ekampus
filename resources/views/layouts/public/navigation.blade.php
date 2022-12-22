813abe4ec88af68797ce7f7cfd56378898bc7e1a
SyafiqCheNor <syafiqchenor@gmail.com>
2022-12-22 11:24:34 +0800

    menu front page

--- resources/views/layouts/public/navigation.blade.php
+++ resources/views/layouts/public/navigation.blade.php
@@ -5,21 +5,69 @@
             <!--begin::Menu item-->
             <div class="menu-item">
                 <!--begin::Menu link-->
-                <a class="menu-link nav-link active py-3 px-4 px-xxl-6" href="#kt_body" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">Home</a>
+                <a class="menu-link nav-link active py-3 px-4 px-xxl-6" href="#kt_body" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">Utama</a>
                 <!--end::Menu link-->
             </div>
             <!--end::Menu item-->
             <!--begin::Menu item-->
             <div class="menu-item">
                 <!--begin::Menu link-->
-                <a class="menu-link nav-link py-3 px-4 px-xxl-6" href="#how-it-works" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">How it Works</a>
+                <a class="menu-link nav-link py-3 px-4 px-xxl-6" href="#how-it-works" data-kt-menu-trigger="hover" data-kt-menu-placement="bottom-start">Permohonan
+                    <span class="svg-icon svg-icon-3 rotate-180 ms-3 me-0">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="currentColor"></path>
+                        </svg>
+                    </span>
+                </a>
                 <!--end::Menu link-->
+                <!--begin::Menu-->
+                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-400px" data-kt-menu="true">
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3">
+                        <a href="{{ route('public.permohonan.index') }}" class="menu-link px-3">
+                            Permohonan Baru
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3">
+                        <a href="#" class="menu-link px-3">
+                            Semakan Permohonan
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3">
+                        <a href="#" class="menu-link px-3">
+                            Temuduga
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3">
+                        <a href="#" class="menu-link px-3">
+                            Rayuan
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3">
+                        <a href="#" class="menu-link px-3">
+                            Semakan Rayuan
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+                </div>
+                <!--end::Menu-->
             </div>
             <!--end::Menu item-->
             <!--begin::Menu item-->
             <div class="menu-item">
                 <!--begin::Menu link-->
-                <a class="menu-link nav-link py-3 px-4 px-xxl-6" href="#achievements" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">Achievements</a>
+                <a class="menu-link nav-link py-3 px-4 px-xxl-6" href="#achievements" data-kt-scroll-toggle="true" data-kt-drawer-dismiss="true">Aduan</a>
                 <!--end::Menu link-->
             </div>
             <!--end::Menu item-->
