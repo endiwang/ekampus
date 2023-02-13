e6a8309e231871a8bda1a53cbc63c9fecc6e5860
SyafiqCheNor <syafiqchenor@gmail.com>
2023-02-13 16:51:45 +0800

    senarai tapisan

---
+++ resources/views/pages/pengurusan/kbg/senarai_permohonan/edit.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.master.main')
+@section('css')
+@endsection
+@section('content')
+    <div id="kt_app_content" class="app-content flex-column-fluid">
+        <div id="kt_app_content_container" class="app-container container-xxl">
+
+            <!--begin::Row-->
+            <div class="row g-5 g-xl-10 mb-5 mb-xl-10">
+                <div class="col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div class="card">
+                        <div class="card-body py-5">
+                            @include('pages.pengurusan.kbg.senarai_permohonan.borang.a_maklumat_pemohon')
+                            @include('pages.pengurusan.kbg.senarai_permohonan.borang.b_tempat_temuduga')
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end::Row-->
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+<script>
+        $("#tarikh_lahir").daterangepicker({
+        autoApply : true,
+        singleDatePicker: true,
+        showDropdowns: true,
+        autoUpdateInput: false,
+        minYear: parseInt(moment().subtract(30,'y').format("YYYY")),
+        maxYear: parseInt(moment().format("YYYY")),
+        locale: {
+            format: 'DD/MM/YYYY'
+        }
+    },function(start, end, label) {
+        var datePicked = moment(start).format('DD/MM/YYYY');
+        $("#tarikh_lahir").val(datePicked);
+    });
+</script>
+@endpush
