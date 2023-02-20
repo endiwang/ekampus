ed952a3bb791dbc69d1002ca435af8407f735e94
suraiyamusfirah <tritiumland12@gmail.com>
2023-02-20 23:42:37 +0800

    :sparkles: module pertukaran syukbah

--- app/Models/PermohonanPertukaranSyukbah.php
+++ app/Models/PermohonanPertukaranSyukbah.php
@@ -11,4 +11,24 @@ class PermohonanPertukaranSyukbah extends Model
 
     protected $table = "permohonan_pertukaran_syukbah";
     protected $guarded = ['id'];
+
+    public function pelajar()
+    {
+        return $this->belongsTo(Pelajar::class, 'pelajar_id', 'id');
+    }
+
+    public function newSyukbah()
+    {
+        return $this->belongsTo(Syukbah::class, 'new_syukbah_id', 'id');
+    }
+
+    public function oldSyukbah()
+    {
+        return $this->belongsTo(Syukbah::class, 'old_syukbah_id', 'id');
+    }
+
+    public function semester()
+    {
+        return $this->belongsTo(Semester::class, 'semester_id', 'id');
+    }
 }
