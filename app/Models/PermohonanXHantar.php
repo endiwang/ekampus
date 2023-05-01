9c2ace81141207f0f3ee79dd21a062d1dd35d147
SyafiqCheNor <syafiqchenor@gmail.com>
2023-05-02 01:35:15 +0800

    permohonan tak hantar

---
+++ app/Models/PermohonanXHantar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PermohonanXHantar extends Model
+{
+    use HasFactory;
+    protected $table = "permohonan_x_hantar";
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2023_04_27_154422_create_permohonan_x_hantar_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permohonan_x_hantar', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pemohon_id')->nullable();
+            $table->string('gambar')->nullable();
+            $table->string('nama_pemohon')->nullable();
+            $table->string('nama_jawi')->nullable();
+            $table->string('no_kp')->nullable();
+            $table->date('tarikh_lahir')->nullable();
+            $table->mediumText('alamat_tetap')->nullable();
+            $table->string('bandar_tetap')->nullable();
+            $table->string('poskod_tetap')->nullable();
+            $table->string('negeri_tetap')->nullable();
+            $table->mediumText('alamat_surat')->nullable();
+            $table->string('bandar_surat')->nullable();
+            $table->string('poskod_surat')->nullable();
+            $table->string('negeri_surat')->nullable();
+            $table->string('no_telefon')->nullable();
+            $table->string('jantina')->nullable();
+            $table->string('nageri_kelahiran')->nullable();
+            $table->string('keturunan')->nullable();
+            $table->string('bumiputra')->nullable();
+            $table->string('mualaf')->nullable();
+            $table->string('kewarganegaraan')->nullable();
+            $table->string('kedaaan_fizikal')->nullable();
+            $table->string('penyakit_kronik')->nullable();
+            $table->string('rekod_kemasukan_wad')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permohonan_x_hantar');
+    }
+};
