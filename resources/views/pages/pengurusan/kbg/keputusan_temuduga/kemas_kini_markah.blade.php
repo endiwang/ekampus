1ed567bb8c7052d21e6a95e6d5db38083c202ab6
SyafiqCheNor <syafiqchenor@gmail.com>
2023-05-12 00:28:52 +0800

    update markah temuduga

--- resources/views/pages/pengurusan/kbg/keputusan_temuduga/kemas_kini_markah.blade.php
+++ resources/views/pages/pengurusan/kbg/keputusan_temuduga/kemas_kini_markah.blade.php
@@ -133,21 +133,23 @@
                             <table id="senarai_pemohon_table" class="table table-striped table-row-bordered gy-5 gs-7 border rounded">
                                 <thead>
                                 <tr class="">
-                                    <th class="w-2% pe-2">Bil</th>
-                                    <th class="w-20%">Nama Pemohon</th>
-                                    <th class="w-10% pe-2">L/P</th>
-                                    <th class="text-center w-10%">Hafazan<br>(50%)</th>
-                                    <th class="text-center w-10%">Tajwid<br>(20%)</th>
-                                    <th class="text-center w-10%">Penampilan<br>(10%)</th>
-                                    <th class="text-center w-10%">Pencapaian<br>Akademik<br>(20%)</th>
-                                    <th class="text-center w-10%">Jumlah</th>
-                                    <th class="text-center w-10%">%</th>
-                                    <th class="text-center w-30%">Catatan / Ulasan
+                                    <th class="pe-2">Bil</th>
+                                    <th class="">Nama Pemohon</th>
+                                    <th class=" pe-2">L/P</th>
+                                    <th class="text-center">Hafazan<br>(50%)</th>
+                                    <th class="text-center">Tajwid<br>(20%)</th>
+                                    <th class="text-center">Penampilan<br>(10%)</th>
+                                    <th class="text-center">Pencapaian<br>Akademik<br>(20%)</th>
+                                    <th class="text-center">Jumlah<br>(100%)</th>
+                                    {{-- <th class="text-center w-10%">%</th> --}}
+                                    <th class="text-center">Catatan / Ulasan
                                     </th>
                                 </tr>
                                 </thead>
                                 <tbody class="">
                                     @foreach ($markah_temuduga as $index=>$data)
+                                        <input type="hidden" size="5" name="id" id="data_id{{ $data->id }}" value="{{ $data->id }}">
+
                                         <tr>
                                             <td>{{ $index+1 }}</td>
                                             <td>{{ $data->pemohon->nama }}<br>{{ $data->pemohon->no_ic }}</td>
@@ -156,8 +158,8 @@
                                             <td><input type="text" size="5" name="tajwid" id="tajwid{{ $data->id }}" value="{{ $data->tajwid}}" style="text-align:center" class="form-control form-control-sm" onchange="kira_markah({{ $data->id }})"></td>
                                             <td><input type="text" size="5" name="akhlak" id="akhlak{{ $data->id }}" value="{{ $data->akhlak}}" style="text-align:center" class="form-control form-control-sm" onchange="kira_markah({{ $data->id }})"></td>
                                             <td><input type="text" size="5" name="akademik" id="akademik{{ $data->id }}" value="{{ $data->akademik}}" style="text-align:center" class="form-control form-control-sm" onchange="kira_markah({{ $data->id }})"></td>
-                                            <td><input type="text" size="5" name="jumlah" id="jumlah{{ $data->id }}" value="{{ $data->jumlah}}" style="text-align:center" class="form-control form-control-sm" onchange="kira_markah({{ $data->id }})"></td>
-                                            <td><input type="text" size="5" name="percentage" id="percentage{{ $data->id }}" value="{{ $data->pcts}}" style="text-align:center" class="form-control form-control-sm" onchange="kira_markah({{ $data->id }})"></td>
+                                            <td><input type="text" size="5" name="jumlah" id="jumlah{{ $data->id }}" value="{{ $data->jumlah}}" style="text-align:center" class="form-control form-control-sm" disabled></td>
+                                            {{-- <td><input type="text" size="5" name="percentage" id="percentage{{ $data->id }}" value="{{ $data->pcts}}" style="text-align:center" class="form-control form-control-sm" onchange="kira_markah({{ $data->id }})"></td> --}}
                                             <td><input type="text" name="catatan" id="catatan{{ $data->id }}" value="{{ $data->catatan}}" style="text-align:center" class="form-control form-control-sm" onchange="kira_markah({{ $data->id }})"></td>
                                         </tr>
                                     @endforeach
@@ -194,29 +196,205 @@
 
 function kira_markah(id)
     {
-        var hafazan     =   parseInt($('#hafazan'+id).val());
+        var jumlah      =   $('#jumlah'+id).val();
+
+        var hafazan = parseInt($('#hafazan'+id).val());
+        $('#hafazan'+id).val($('#hafazan'+id).val().replace(/[^0-9]/g, ''));
+
+        if(hafazan > 50 )
+        {
+            alert('Hafazan tidak boleh melebihi 50%')
+            $('#hafazan'+id).val(0);
+            hafazan = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+
+        }else if(isNaN(hafazan))
+        {
+            $('#hafazan'+id).val(0);
+            hafazan = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+        }else{
+
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+            $.ajax({
+                    url: "{{route('pengurusan.kbg.keputusan_temuduga.store')}}",
+                    type: "POST",
+                    data: {
+                                id: id,
+                                hafazan: $('#hafazan'+id).val(),
+                                tajwid: $('#tajwid'+id).val(),
+                                akhlak: $('#akhlak'+id).val(),
+                                akademik: $('#akademik'+id).val(),
+                                jumlah: isNaN($('#jumlah'+id).val()) ? 0 :$('#jumlah'+id).val(),
+                                catatan: $('#catatan'+id).val(),
+                                _token: '{{csrf_token()}}'
+                            },
+                    dataType: 'json',
+                    processResults: function (data) {
+                    // Transforms the top-level key of the response object from 'items' to 'results'
+                    return {
+                        results: data
+                    };
+                    }
+                })
+
+        }
+
         var tajwid      =   parseInt($('#tajwid'+id).val());
+        $('#tajwid'+id).val($('#tajwid'+id).val().replace(/[^0-9]/g, ''));
+
+        if(tajwid > 20 )
+        {
+            alert('Tajwid tidak boleh melebihi 20%')
+            $('#tajwid'+id).val(0);
+            tajwid = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+
+        }else if(isNaN(tajwid))
+        {
+            $('#tajwid'+id).val(0);
+            tajwid = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+
+        }else{
+
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+            $.ajax({
+                    url: "{{route('pengurusan.kbg.keputusan_temuduga.store')}}",
+                    type: "POST",
+                    data: {
+                                id: id,
+                                hafazan: $('#hafazan'+id).val(),
+                                tajwid: $('#tajwid'+id).val(),
+                                akhlak: $('#akhlak'+id).val(),
+                                akademik: $('#akademik'+id).val(),
+                                jumlah: isNaN($('#jumlah'+id).val()) ? 0 :$('#jumlah'+id).val(),
+                                catatan: $('#catatan'+id).val(),
+                                _token: '{{csrf_token()}}'
+                            },
+                    dataType: 'json',
+                    processResults: function (data) {
+                    // Transforms the top-level key of the response object from 'items' to 'results'
+                    return {
+                        results: data
+                    };
+                    }
+                })
+
+            }
+
         var akhlak      =   parseInt($('#akhlak'+id).val());
+        $('#akhlak'+id).val($('#akhlak'+id).val().replace(/[^0-9]/g, ''));
+
+        if(akhlak > 10 )
+        {
+            alert('Akhlak tidak boleh melebihi 10%')
+            $('#akhlak'+id).val(0);
+            akhlak = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+
+        }else if(isNaN(akhlak))
+        {
+            $('#akhlak'+id).val(0);
+            akhlak = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+
+        }else{
+
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+            $.ajax({
+                    url: "{{route('pengurusan.kbg.keputusan_temuduga.store')}}",
+                    type: "POST",
+                    data: {
+                                id: id,
+                                hafazan: $('#hafazan'+id).val(),
+                                tajwid: $('#tajwid'+id).val(),
+                                akhlak: $('#akhlak'+id).val(),
+                                akademik: $('#akademik'+id).val(),
+                                jumlah: isNaN($('#jumlah'+id).val()) ? 0 :$('#jumlah'+id).val(),
+                                catatan: $('#catatan'+id).val(),
+                                _token: '{{csrf_token()}}'
+                            },
+                    dataType: 'json',
+                    processResults: function (data) {
+                    // Transforms the top-level key of the response object from 'items' to 'results'
+                    return {
+                        results: data
+                    };
+                    }
+                })
+
+            }
+
         var akademik    =   parseInt($('#akademik'+id).val());
-        var percentage  =   parseInt($('#percentage'+id).val());
-        var catatan     =   parseInt($('#catatan'+id).val());
+        $('#akademik'+id).val($('#akademik'+id).val().replace(/[^0-9]/g, ''));
 
-        var jumlah      = 0;
+        if(akademik > 20 )
+        {
+            alert('Akademik tidak boleh melebihi 20%')
+            $('#akademik'+id).val(0);
+            akademik = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
 
-        if(hafazan > 50)
 
-        jumlah = hafazan+tajwid+akhlak+akademik;
-        $('#jumlah'+id).val(jumlah);
+        }else if(isNaN(akademik))
+        {
+            $('#akademik'+id).val(0);
+            akademik = 0;
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
 
 
-        console.log(hafazan)
-        console.log(tajwid)
-        console.log(akhlak)
-        console.log(akademik)
-        console.log(jumlah)
-        console.log(percentage)
-        console.log(catatan)
+        }else{
+
+            jumlah      =   hafazan+tajwid+akhlak+akademik;
+            $('#jumlah'+id).val(jumlah);
+
+            $.ajax({
+                    url: "{{route('pengurusan.kbg.keputusan_temuduga.store')}}",
+                    type: "POST",
+                    data: {
+                                id: id,
+                                hafazan: $('#hafazan'+id).val(),
+                                tajwid: $('#tajwid'+id).val(),
+                                akhlak: $('#akhlak'+id).val(),
+                                akademik: $('#akademik'+id).val(),
+                                jumlah: isNaN($('#jumlah'+id).val()) ? 0 :$('#jumlah'+id).val(),
+                                catatan: $('#catatan'+id).val(),
+                                _token: '{{csrf_token()}}'
+                            },
+                    dataType: 'json',
+                    processResults: function (data) {
+                    // Transforms the top-level key of the response object from 'items' to 'results'
+                    return {
+                        results: data
+                    };
+                    }
+                })
+
+            }
     }
+
+
 </script>
 
 @endpush
--- routes/pengurusan/kbg.php
+++ routes/pengurusan/kbg.php
@@ -17,12 +17,17 @@ use Illuminate\Support\Facades\Route;
     Route::resource('/senarai_permohonan', SenaraiPermohonanController::class)->only(['index'])->name('index','pengurusan.senarai_permohonan.index');
     Route::get('senarai_permohonan/pemohon/{id}', [SenaraiPermohonanController::class, 'edit'])->name('pengurusan.senarai_permohonan.pemohon');
     Route::post('senarai_permohonan/pilih', [SenaraiPermohonanController::class, 'pilih'])->name('pengurusan.senarai_permohonan.pilih');
+
     Route::resource('/senarai_tapisan_permohonan', SenaraiTapisanPermohonanController::class)->only(['index'])->name('index','pengurusan.senarai_tapisan_permohonan.index');
+    Route::get('/senarai_tapisan_permohonan/proses_pemilihan', [SenaraiTapisanPermohonanController::class,'proses_pemilihan'])->name('pengurusan.senarai_tapisan_permohonan.proses_pemilihan');
+
     Route::resource('/proses_temuduga', ProsesTemudugaController::class)->only(['index','create','store'])->name('index','pengurusan.proses_temuduga.index','create','pengurusan.proses_temuduga.create','store','pengurusan.proses_temuduga.store');
     Route::get('/proses_temuduga/{id}/pilih_pemohon', [ProsesTemudugaController::class, 'pilih_pemohon'])->name('pengurusan.proses_temuduga.pilih_pemohon');
     Route::get('/proses_temuduga_api/{id}', [ProsesTemudugaController::class, 'pilih_pemohon_api'])->name('pengurusan.proses_temuduga.pilih_pemohon_api');
     Route::post('/proses_temuduga_store_pemohon', [ProsesTemudugaController::class, 'store_pemohon'])->name('pengurusan.proses_temuduga.store_pemohon');
-    Route::resource('/keputusan_temuduga', KeputusanTemudugaController::class)->only(['index'])->name('index','pengurusan.keputusan_temuduga.index');
+
+    Route::resource('/keputusan_temuduga', KeputusanTemudugaController::class)->only(['index','store'])->name('index','pengurusan.keputusan_temuduga.index','store','pengurusan.keputusan_temuduga.store');
+
     Route::get('/keputusan_temuduga/{id}/kemas_kini_markah', [KeputusanTemudugaController::class,'kemas_kini_markah'])->name('pengurusan.keputusan_temuduga.kemas_kini_markah');
 
 
