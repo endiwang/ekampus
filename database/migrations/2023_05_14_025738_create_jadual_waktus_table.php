d84fa88f6a48565d72cc8c3000a5c025de6e1cb9
suraiyamusfirah <tritiumland12@gmail.com>
2023-05-14 11:13:09 +0800

    :hammer: fix migration

--- database/migrations/2023_05_14_025738_create_jadual_waktus_table.php
+++ database/migrations/2023_05_14_025738_create_jadual_waktus_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('jadual_waktus', function (Blueprint $table) {
+        Schema::create('jadual_waktu', function (Blueprint $table) {
             $table->id();
+            $table->integer('kelas_id')->nullable();
+            $table->integer('pengajian_id')->nullable();
+            $table->integer('status')->nullable()->comment('1-Belum Disahkan, 2-Telah Disahkan');
+            $table->integer('created_by')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jadual_waktus');
+        Schema::dropIfExists('jadual_waktu');
     }
 };
