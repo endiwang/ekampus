924eff3028657b668d82c7f6a2801c2c48be4ee0
SyafiqCheNor <syafiqchenor@gmail.com>
2023-07-19 01:04:37 +0800

    tetapan keluar masuk

--- resources/views/layouts/master/sidebar/hep.blade.php
+++ resources/views/layouts/master/sidebar/hep.blade.php
@@ -77,6 +77,33 @@
                     </div>
                 </div>
             </div>
+
+            <div data-kt-menu-trigger="click" class="menu-item {{ (request()->is('utama')) ? 'here show' : '' }} menu-accordion">
+                <span class="menu-link">
+                    <span class="menu-icon">
+                        <span class="svg-icon svg-icon-2">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="2" y="2" width="9" height="9" rx="2" fill="currentColor" />
+                                <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="currentColor" />
+                                <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="currentColor" />
+                                <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="currentColor" />
+                            </svg>
+                        </span>
+                    </span>
+                    <span class="menu-title">Tetapan</span>
+                    <span class="menu-arrow"></span>
+                </span>
+                <div class="menu-sub menu-sub-accordion">
+                    <div class="menu-item">
+                        <a class="menu-link {{ Request::routeIs('pengurusan.hep.tetapan.keluar_masuk.*') ? 'active' : '' }}" href="{{ route('pengurusan.hep.tetapan.keluar_masuk.index') }}">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Keluar Masuk</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- routes/pengurusan/hep.php
+++ routes/pengurusan/hep.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\Pengurusan\HEP\MainHEPController;
 use App\Http\Controllers\Pengurusan\HEP\SahsiahDisiplin\KeluarMasukPelajarController;
+use App\Http\Controllers\Pengurusan\HEP\SahsiahDisiplin\TetapanKeluarMasukController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/', MainHEPController::class)->only(['index',]);
 
 Route::group(['prefix'=>'permohonan','as'=>'permohonan.'], function(){
     Route::resource('keluar_masuk', KeluarMasukPelajarController::class);
+});
+
+Route::group(['prefix'=>'tetapan','as'=>'tetapan.'], function(){
+    Route::resource('keluar_masuk', TetapanKeluarMasukController::class);
 });
