909db3c77bf237429ac019098e9f4e87a9093023
Azwar Azahar <muhdazwar94@gmail.com>
2023-08-09 16:12:04 +0800

    add soft delete

--- app/Models/AduanPenyelenggaraan.php
+++ app/Models/AduanPenyelenggaraan.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AduanPenyelenggaraan extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    
     protected $table = 'aduan_penyelenggaraan';
     protected $guarded = ['id'];
 
