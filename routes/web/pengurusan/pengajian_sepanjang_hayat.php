7590346c90cd176867f04f43f2fac1d6f29afd71
ibahmaso94 <adibahmansor94@gmail.com>
2023-08-19 18:25:20 +0800

    Tetapan Majlis penyerahan sijil tahfiz
    create controller, model, migration file for Tetapan Majlis penyerahan sijil tahfiz

--- routes/web/pengurusan/pengajian_sepanjang_hayat.php
+++ routes/web/pengurusan/pengajian_sepanjang_hayat.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Pengurusan\PengajianSepanjangHayat\PermohonanController
 use App\Http\Controllers\Pengurusan\PengajianSepanjangHayat\TetapanPenemudugaSijilTahfizController;
 use App\Http\Controllers\Pengurusan\PengajianSepanjangHayat\TetapanPeperiksaanSijilTahfizController;
 use App\Http\Controllers\Pengurusan\PengajianSepanjangHayat\TetapanPusatPeperiksaanController;
+use App\Http\Controllers\Pengurusan\PengajianSepanjangHayat\TetapanMajlisPenyerahanSijilTahfizController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>'tetapan','as'=>'tetapan.'], function(){
@@ -16,6 +17,7 @@ Route::group(['prefix'=>'tetapan','as'=>'tetapan.'], function(){
         Route::get('fetchPusatPeperiksaanNegeri', [TetapanPenemudugaSijilTahfizController::class, 'fetchPusatPeperiksaanNegeri'])->name('fetchPusatPeperiksaan.negeri');
     });
     Route::resource('penemuduga_sijil_tahfiz', TetapanPenemudugaSijilTahfizController::class);
+    Route::resource('majlis_penyerahan_sijil_tahfiz', TetapanMajlisPenyerahanSijilTahfizController::class);
 });
 
 Route::group(['prefix'=>'proses_permohonan','as'=>'proses_permohonan.'], function(){
