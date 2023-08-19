d272c46ba1dcf4929c7c4700d0b29e5ddb926f05
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-08-19 21:50:51 +0800

    wip

--- routes/web/pusat-islam.php
+++ routes/web/pusat-islam.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\PusatIslam\RekodKehadiranController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth'])
-    ->group(function() {
+    ->group(function () {
         Route::get('pusat-islam/dashboard', DashboardController::class)
             ->name('pusat-islam.dashboard.index');
 
