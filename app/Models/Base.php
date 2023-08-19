dbee38bd81182b28d66dcb1a56cb4e38901340a3
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-08-20 00:43:46 +0800

    Install Laravel Auditing & Added Base Model

---
+++ app/Models/Base.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Auditable as AuditableTrait;
+use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
+
+class Base extends Model implements AuditableContract, HasMedia
+{
+    use AuditableTrait;
+    use HasFactory;
+}
