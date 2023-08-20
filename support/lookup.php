dbed1a481a46d545fe3f69c24655c2ba56a45942
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-08-20 09:29:20 +0800

    Update

--- support/lookup.php
+++ support/lookup.php
@@ -4,12 +4,16 @@ use App\Models\Lookup;
 use Illuminate\Support\Facades\Cache;
 
 if (! function_exists('lookup')) {
-    function lookup(string $category, string $key, int $ttl = 60)
+    function lookup(string $category, string $key, int $ttl = 60, bool $is_enabled = true)
     {
         return Cache::remember(
             "lookup.{$category}.{$key}.{$ttl}",
             $ttl,
-            fn () => Lookup::where('key', $key)->where('category', $category)->get()
+            fn () => Lookup::query()
+                ->where('key', $key)
+                ->where('category', $category)
+                ->where('is_enabled', $is_enabled)
+                ->get()
         );
     }
 }
