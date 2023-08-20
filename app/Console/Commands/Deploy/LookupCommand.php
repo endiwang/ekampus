94f183740b4cc20bb524bf6fdfde480b7f0db327
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-08-20 09:53:43 +0800

    Fixed class anme

--- app/Console/Commands/Deploy/LookupCommand.php
+++ app/Console/Commands/Deploy/LookupCommand.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands\Deploy;
 
 use Illuminate\Console\Command;
 
-class KaunselingCommand extends Command
+class LookupCommand extends Command
 {
     /**
      * The name and signature of the console command.
