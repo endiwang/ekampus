98a1945ae1438162412d0dbbda07483125a6d212
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-08-21 08:32:08 +0800

    Register datatable routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 require_all_in(base_path('routes/web/*.php'));
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum', 'ajax'])
+    ->prefix('datatable')
+    ->as('dt.')
+    ->group(function () {
+        require_all_in(base_path('routes/datatable/*.php'));
+    });
