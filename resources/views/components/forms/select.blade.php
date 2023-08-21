178d4e748c3f6a238aa95c8a8cd1050b852cbf7a
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-08-21 10:42:45 +0800

    Added common form component

---
+++ resources/views/components/forms/select.blade.php
@@ -0,0 +1,13 @@
+@props(['placeholder' => 'Sila Pilih', 'label' => 'Label', 'key' => 'key', 'options' => [], 'description' => '', 'value' => ''])
+
+<div class="row mb-6">
+    <div class="col-lg-4">
+        {{ Form::label($key, $label, ['class' => 'col-form-label required fw-semibold fs-6 pb-0 pt-0']) }}
+        @if ($description)
+            <div class="form-text mt-0">{{ $description }}</div>
+        @endif
+    </div>
+    <div class="col-lg-8 fv-row">
+        {{ Form::select($key, $options, $value, ['placeholder' => $placeholder, 'class' => 'form-control']) }}
+    </div>
+</div>
---
+++ resources/views/components/forms/text.blade.php
@@ -0,0 +1,16 @@
+@props(['label' => 'Label', 'key' => 'key', 'description' => '', 'info' => '', 'value' => ''])
+
+<div class="row mb-6">
+    <div class="col-lg-4">
+        {{ Form::label($key, $label, ['class' => 'col-form-label required fw-semibold fs-6 pb-0 pt-0']) }}
+        @if ($description)
+            <div class="form-text mt-0">{{ $description }}</div>
+        @endif
+    </div>
+    <div class="col-lg-8 fv-row">
+        {{ Form::text($key, $value, ['class' => 'form-control']) }}
+        @if($info)
+            <div class="form-text">{{ $info }}</div>
+        @endif
+    </div>
+</div>
