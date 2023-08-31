d8d3896a3078e52fa6fc054b7f145b40c9fe7aed
shahiran <shahiran@vialing.com>
2023-08-31 15:29:35 +0800

    add: submodule laporan

--- resources/views/layouts/master/sidebar/jabatan_pengajian_sepanjang_hayat.blade.php
+++ resources/views/layouts/master/sidebar/jabatan_pengajian_sepanjang_hayat.blade.php
@@ -25,7 +25,7 @@
                 <!--end:Menu link-->
             </div>
 
-            <div data-kt-menu-trigger="click" class="menu-item {{ (request()->is('utama')) ? 'here show' : '' }} menu-accordion">
+            <div data-kt-menu-trigger="click" class="menu-item {{ Request::routeIs('pengurusan.pengajian_sepanjang_hayat.tetapan.*') ? 'here show' : '' }} menu-accordion">
                 <span class="menu-link">
                     <span class="menu-icon">
                         <span class="svg-icon svg-icon-2">
@@ -117,7 +117,7 @@
                     </div>
                 </div>
             </div>
-            <div data-kt-menu-trigger="click" class="menu-item {{ (request()->is('utama')) ? 'here show' : '' }} menu-accordion">
+            <div data-kt-menu-trigger="click" class="menu-item {{ Request::routeIs('pengurusan.pengajian_sepanjang_hayat.pemarkahan.*') ? 'here show' : '' }} menu-accordion">
                 <span class="menu-link">
                     <span class="menu-icon">
                         <span class="svg-icon svg-icon-2">
@@ -151,7 +151,7 @@
                     </div>
                 </div>
             </div>
-            <div data-kt-menu-trigger="click" class="menu-item {{ (request()->is('utama')) ? 'here show' : '' }} menu-accordion">
+            <div data-kt-menu-trigger="click" class="menu-item {{ Request::routeIs('pengurusan.pengajian_sepanjang_hayat.pengurusan_sijil_tahfiz.*') ? 'here show' : '' }} menu-accordion">
                 <span class="menu-link">
                     <span class="menu-icon">
                         <span class="svg-icon svg-icon-2">
@@ -177,7 +177,7 @@
                     </div>
                 </div>
             </div>
-            <div data-kt-menu-trigger="click" class="menu-item {{ (request()->is('utama')) ? 'here show' : '' }} menu-accordion">
+            <div data-kt-menu-trigger="click" class="menu-item {{ Request::routeIs('pengurusan.pengajian_sepanjang_hayat.jemputan.*') ? 'here show' : '' }} menu-accordion">
                 <span class="menu-link">
                     <span class="menu-icon">
                         <span class="svg-icon svg-icon-2">
@@ -199,6 +199,32 @@
                                 <span class="bullet bullet-dot"></span>
                             </span>
                             <span class="menu-title">Jemputan Majlis Pensijilan</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div data-kt-menu-trigger="click" class="menu-item {{ Request::routeIs('pengurusan.pengajian_sepanjang_hayat.laporan.*') ? 'here show' : '' }} menu-accordion">
+                <span class="menu-link">
+                    <span class="menu-icon">
+                        <span class="svg-icon svg-icon-2">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="2" y="2" width="9" height="9" rx="2" fill="currentColor" />
+                                <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="currentColor" />
+                                <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="currentColor" />
+                                <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="currentColor" />
+                            </svg>
+                        </span>
+                    </span>
+                    <span class="menu-title">Laporan</span>
+                    <span class="menu-arrow"></span>
+                </span>
+                <div class="menu-sub menu-sub-accordion">
+                    <div class="menu-item">
+                        <a class="menu-link {{ Request::routeIs('pengurusan.pengajian_sepanjang_hayat.laporan.rekod_analisa.*') ? 'active' : '' }}" href="{{ route('pengurusan.pengajian_sepanjang_hayat.laporan.rekod_analisa.index') }}">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Rekod Analisa Zon Peperiksaan</span>
                         </a>
                     </div>
                 </div>
