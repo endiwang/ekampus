854262007c8db3e0c18fe51f2652131cb002d97c
SyafiqCheNor <syafiqchenor@gmail.com>
2023-09-04 02:48:40 +0800

    migration bilik asrama

---
+++ app/Models/OldDatabase/sis_a_tblbilik.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\OldDatabase;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class sis_a_tblbilik extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mysql2';
+
+    protected $table = '_sis_a_tblbilik';
+}
