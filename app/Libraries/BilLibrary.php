b700c4f3c475677981ac00ab702ae961df98f69d
Azwar Azahar <muhdazwar94@gmail.com>
2023-09-06 08:50:57 +0800

    update bil library to handle bil details and bil peperiksaan

--- app/Libraries/BilLibrary.php
+++ app/Libraries/BilLibrary.php
@@ -29,6 +29,8 @@ class BilLibrary {
             $bil->yuran_id = $data['yuran']->id;
             $bil->description = $data['yuran']->nama;
             $bil->status = 1;
+            $bil->id_hash = md5($bil->id . now());
+
             if($bil->save())
             {
                 $amaun_total = 0;
@@ -48,8 +50,19 @@ class BilLibrary {
                     }
                 }
                 else {
-                    $yuran_detail = YuranDetail::where('yuran_id', $data['yuran']->id);
-                    exit;
+                    $yuran_detail_senarai = YuranDetail::where('yuran_id', $data['yuran']->id)->get();
+                    foreach($yuran_detail_senarai as $key => $yuran_detail)
+                    {
+                        $bil_detail = new BilDetail;
+                        $bil_detail->bil_id = $bil->id;
+                        $bil_detail->yuran_id = $bil->yuran_id;
+                        $bil_detail->description = $yuran_detail->nama;
+                        $bil_detail->amaun = $yuran_detail->amaun;
+                        $bil_detail->save();
+
+                        $amaun_total += $bil_detail->amaun;
+
+                    }
                 }
 
                 $bil->amaun = $amaun_total;
@@ -58,6 +71,62 @@ class BilLibrary {
             }
 
             event(new BilYuranEvent($bil));
+        }
+        
+    }
+
+    public static function createBilPeperikSaan($data)
+    {
+        if(!empty($data['yuran']) && isset($data['caj_peperiksaan_pengurusan']) && isset($data['caj_peperiksaan_subjek']))
+        {
+            $count_bil = Bil::count();
+            $no_bil = sprintf('%04d', $count_bil + 1);
+
+            $bil = new Bil;
+            $bil->doc_no = 'INV' . $no_bil;
+            $bil->pelajar_id = $data['pelajar']->pelajar_id;            
+            $bil->yuran_id = $data['yuran']->id;
+            $bil->description = $data['yuran']->nama;
+            $bil->status = 1;
+
+            if($bil->save())
+            {
+                $amaun_total = 0;
+
+                if(count($data['caj_peperiksaan_pengurusan']) > 0)
+                {
+                    foreach($data['caj_peperiksaan_pengurusan'] as $caj)
+                    {
+                        $bil_detail = new BilDetail;
+                        $bil_detail->bil_id = $bil->id;
+                        $bil_detail->yuran_id = $bil->yuran_id;
+                        $bil_detail->description = ucwords(str_replace('_', ' ', $caj->description));
+                        $bil_detail->amaun = $caj->jumlah;
+                        $bil_detail->save();
+
+                        $amaun_total += $bil_detail->amaun;
+                    }
+                }
+
+                if(count($data['caj_peperiksaan_subjek']) > 0)
+                {
+                    foreach($data['caj_peperiksaan_subjek'] as $caj)
+                    {
+                        $bil_detail = new BilDetail;
+                        $bil_detail->bil_id = $bil->id;
+                        $bil_detail->yuran_id = $bil->yuran_id;
+                        $bil_detail->description = ucwords(str_replace('_', ' ', $caj->description)) . ' - ' . @$caj->subjek->nama;
+                        $bil_detail->amaun = $caj->jumlah;
+                        $bil_detail->save();
+
+                        $amaun_total += $bil_detail->amaun;
+                    }
+                }
+
+                $bil->amaun = $amaun_total;
+                $bil->save();
+
+            }
         }
         
     }
