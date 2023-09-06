ff6049ca10e3de8fccc7e07cc352a8071b002bc1
Azwar Azahar <muhdazwar94@gmail.com>
2023-09-06 09:05:22 +0800

    add seeder for yuran detail

--- database/migrations/2023_09_03_133640_create_yuran_detail_table.php
+++ database/migrations/2023_09_03_133640_create_yuran_detail_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,6 +22,9 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        Artisan::call('db:seed', array('--class' => 'YuranDetailSeeder'));
+
     }
 
     /**
