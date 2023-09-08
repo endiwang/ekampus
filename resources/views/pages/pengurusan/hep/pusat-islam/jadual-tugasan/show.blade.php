f31f73169591a33e492e8ad559eae19b4d4c9cb1
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-09-08 19:22:40 +0800

    Added Tugasan View

--- resources/views/pages/pengurusan/hep/pusat-islam/jadual-tugasan/show.blade.php
+++ resources/views/pages/pengurusan/hep/pusat-islam/jadual-tugasan/show.blade.php
@@ -9,6 +9,21 @@
 @extends('layouts.master.main')
 @section('content')
     <x-container>
+        <div class="mt-8">
+            <h4>Jenis Tugasan</h3>
+            @include('pages.pengurusan.hep.pusat-islam.partials.jenis-tugasan')
+        </div>
+
+        <div class="mt-8">
+            <h4>Waktu Solat</h3>
+            @include('pages.pengurusan.hep.pusat-islam.partials.waktu-solat')
+        </div>
+
+        <div class="mt-8">
+            <h4>Maklumat Petugas</h4>
+            @include('pages.pengurusan.hep.pusat-islam.partials.user')
+        </div>
+
 
     </x-container>
 @endsection
